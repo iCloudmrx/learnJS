8445e84e1c12de8964b6e777fbec8058067b5289
iCloudmrx <mahmudjonov2001uz@gmail.com>
2023-03-20 16:45:15 +0500

    create html css and js file

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+// har doim js kodni shu tagni ichida yozish kerak sabab serverdan hamma malumotlarni olib bo'lib keyin saytni ochadi
+document.addEventListener("DOMContentLoaded", () => {});
+const adv = document.querySelector(".promo__adv"),
+    genre = document.querySelector(".promo__genre"),
+    bg = document.querySelector(".promo__bg"),
+    list = document.querySelector(".promo__interactive-list");
+
+const seriesDB = {
+    series: [
+        "Omar",
+        "The Final Legacy",
+        "Ertugrul",
+        "Magnificent Century",
+        "The Great Seljuks: Guardians...",
+    ],
+};
+adv.remove();
+genre.textContent = "Comedy";
+bg.style.backgroundImage = 'url("img/1.jpg")';
+list.innerHTML = "";
+seriesDB.series.forEach((item, idx) => {
+    list.innerHTML += `<li class="promo__interactive-item">
+    ${idx + 1} ${item}
+    <div class="delete"></div>
+</li>`;
+});
